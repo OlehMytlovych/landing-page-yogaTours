cd3c9fdc76d88cc4f8f469b79c5b0b57189bd933
Oleh Mytlovych <bigbang007700@gmail.com>
2019-05-15 20:09:30 +0300

    Modal window has been spread on the nodeList of buttons in the tabs

--- js/script.js
+++ js/script.js
@@ -95,18 +95,27 @@ window.addEventListener("DOMContentLoaded", function (){
   //MODAL WINDOW
 
   let moreButton = document.querySelector('.more'),//The "Узнать больше" button
+      descriptionBtn = document.querySelectorAll('.description-btn'),//the buttons from the tabs
       overlay = document.querySelector('.overlay'),//the modal window
       closeCross = document.querySelector('.popup-close');
+  
+  descriptionBtn = Array.from(descriptionBtn);//making an array from the nodelist to push the moreButton
+  descriptionBtn.push(moreButton);
 
-  moreButton.addEventListener('click', function () {
-    overlay.style.display = 'block';
-    this.classList.add('more-splash');//this means current element (similar to event.target somehow)
-    document.body.style.overflow = 'hidden';//to disable scroll on the page while the modal window is visible 
+
+  descriptionBtn.forEach(function(el){
+    el.addEventListener('click', function () {
+      overlay.style.display = 'block';
+      this.classList.add('more-splash');//this means current element (similar to event.target somehow)
+      document.body.style.overflow = 'hidden';//to disable scroll on the page while the modal window is visible 
+    });
   });
 
   closeCross.addEventListener('click', function() {
     overlay.style.display = 'none';
     moreButton.classList.remove('more-splash');
     document.body.style.overflow = '';
-  })
+  });
+
+  console.log(descriptionBtn)
 });
