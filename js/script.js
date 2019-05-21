f8c904d11784ef79eff561ac73d9ceb1503265d0
Oleh Mytlovych <bigbang007700@gmail.com>
2019-05-21 18:02:30 +0300

    Slider enabled

--- js/script.js
+++ js/script.js
@@ -177,4 +177,51 @@ window.addEventListener("DOMContentLoaded", function (){
                   .then(() => statusMessage.innerHTML = message.success)
                   .catch(() => statusMessage.innerHTML = message.failure);
 
+  // SLIDER
+
+  let slides = document.querySelectorAll('.slider-item'),
+      prev = document.querySelector('.prev'),
+      next = document.querySelector('.next'),
+      dotWrap = document.querySelector('.slider-dots'),
+      dots = document.querySelectorAll('.dot'),
+      sliderIndex = 0;
+
+  dots = Array.from(dots);// to use indexOf when click on a dot
+
+  function showSlide() {
+    // the first slide will be shown when a user clicks right being on the last slide
+    if (sliderIndex == slides.length){
+      sliderIndex = 0;
+    }
+    // the last slide will be shown when a user clicks left being on the first slide
+    if (sliderIndex < 0){
+      sliderIndex = 3;
+    }
+
+    slides.forEach((item) => item.style.display = 'none');
+    slides[sliderIndex].style.display = 'block';
+
+    dots.forEach((item) => item.classList.remove('dot-active'));
+    dots[sliderIndex].classList.add('dot-active');
+    }
+
+  showSlide();// to show only the first slide
+
+  prev.addEventListener('click', function() {
+    sliderIndex--;
+    showSlide();
+  });
+
+  next.addEventListener('click', function() {
+    sliderIndex++;
+    showSlide();
+  });
+
+  dotWrap.addEventListener('click', function(event){
+    if (event.target.classList.contains('dot')){
+    //getting index of the clicked dot and assigning it to the appropriate slide
+    sliderIndex = dots.indexOf(event.target);
+    showSlide();
+    }
+  });
 });
