b4c70ec9c56344753676a0babc30f698610f1583
Oleh Mytlovych <bigbang007700@gmail.com>
2019-05-21 19:16:02 +0300

    Cost calculator added

--- js/script.js
+++ js/script.js
@@ -224,4 +224,53 @@ window.addEventListener("DOMContentLoaded", function (){
     showSlide();
     }
   });
+
+  //CALCULATOR
+
+  let people = document.querySelectorAll('.counter-block-input')[0],
+      days = document.querySelectorAll('.counter-block-input')[1],
+      place = document.querySelector('#select'),
+      totalOutput = document.querySelector('#total'),
+      peopleSum = 0,
+      daysSum = 0,
+      placeIndex = +place.options[0].value,//index given by default
+      totalSum;
+
+  totalOutput.innerHTML = 0;
+  people.value = 0;
+  days.value = 0;
+
+  people.addEventListener('blur', function(){
+    peopleSum = +this.value;
+    totalSum = (peopleSum + daysSum) * 4000 * placeIndex;
+
+    if (days.value == '' || people.value == '' || days.value == '0' || people.value == '0'){
+      totalOutput.innerHTML = 0;
+    } else {
+      totalOutput.innerHTML = totalSum;
+    }
+
+    
+  });
+
+  days.addEventListener('blur', function(){
+    daysSum = +this.value;
+    totalSum = (peopleSum + daysSum) * 4000 * placeIndex;
+
+    if (days.value == '' || people.value == '' || days.value == '0' || people.value == '0'){
+      totalOutput.innerHTML = 0;
+    } else {
+      totalOutput.innerHTML = totalSum;
+    }
+  });
+
+  place.addEventListener('change', function(){
+    if (days.value == '' || people.value == '' || days.value == '0' || people.value == '0'){
+      totalOutput.innerHTML = 0;
+    } else {
+      let a = totalSum;
+      totalOutput.innerHTML = a * this.options[this.selectedIndex].value;
+    }
+    
+  })
 });
