3edd1c6c5d28fa126a29941ce940f98e65fadd83
Oleh Mytlovych <bigbang007700@gmail.com>
2019-05-21 16:45:48 +0300

    AJAX requests re-written to send JSON files

--- js/script.js
+++ js/script.js
@@ -133,7 +133,7 @@ window.addEventListener("DOMContentLoaded", function (){
   form = Array.from(form);
   let i;
   for (i = 0; i < form.length; i++){
-      form[i].addEventListener('submit', sendAJAXRequest)
+      form[i].addEventListener('submit', sendAJAXRequest);
     }
 
   function sendAJAXRequest(event){
@@ -146,17 +146,19 @@ window.addEventListener("DOMContentLoaded", function (){
 
     let request = new XMLHttpRequest();
     request.open('POST', 'server.php');
-    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');// to send json 1)change the heading to:
-    //request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    //request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
     let formData = new FormData(form);
-    //to send json 2) stringify the obj:
-    //let obj = {};
-    //formData.forEach(function(value, key){
-    //  obj[key] = value;
-    //})
-    //let json = JSON.stringify(obj);
-    request.send(formData);//to send json 3) replace this string with request.send(json);
+    //request.send(formData)
+    
+    let obj = {};
+    formData.forEach(function(value, key){
+      obj[key] = value;
+    });
+    let json = JSON.stringify(obj);
+    request.send(json);
 
     request.addEventListener('readystatechange', function(){
      if (request.readyState < 4){
@@ -171,6 +173,6 @@ window.addEventListener("DOMContentLoaded", function (){
     for (let i = 0; i < input.length; i++){
       input[i].value = '';
     }
-}
+  }
 
 });
